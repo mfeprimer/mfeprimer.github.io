e83261a44e8fe26df201281e922a2c4b3ec9672e
Wubin Qu <quwubin@gmail.com>
2019-11-04 01:11:31 +0800

    rebuilding site 2019年11月 4日 星期一 01时11分31秒 CST

---
+++ search.min.9fd28f6300f1140b911711b3748baaeaba78bfe18b0c8ce29d743a0d19855300.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/search-data.min.332607b0c1ae4cabf08808288b6083b29c7a5090469aced61fd31abf402db5ed.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
