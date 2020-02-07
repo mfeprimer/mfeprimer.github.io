3ec4b657a8a2b62c7740830d8fb39dc80e62d6e6
Wubin Qu <quwubin@gmail.com>
2020-02-07 12:39:02 +0800

    rebuilding site 2020年 2月 7日 星期五 12时39分02秒 CST

---
+++ search.min.179e0b32184dcce128f97953260eaa1a99f0c167c9755df31e3f44417aa473d1.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/search-data.min.d636cd14075e33c1dc6825f991d6c168c86a750c9031539aa6f356bf635b1ff4.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
